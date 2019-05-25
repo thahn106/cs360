c34e36d2bd85b89d7ffe6bd5ae1ee7c7574974ca
Sangwoo (Thomas) Hahn <thahn106@gmail.com>
2019-05-25 14:23:13 +0900

    Basic templating.

---
+++ frontend.py
@@ -0,0 +1,44 @@
+# This contains our frontend; since it is a bit messy to use the @app.route
+# decorator style when using application factories, all of our routes are
+# inside blueprints. This is the front-facing blueprint.
+#
+# You can find out more about blueprints at
+# http://flask.pocoo.org/docs/blueprints/
+
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
+from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
+from markupsafe import escape
+
+from .nav import nav
+
+frontend = Blueprint('frontend', __name__)
+
+# We're adding a navbar as well through flask-navbar. In our example, the
+# navbar has an usual amount of Link-Elements, more commonly you will have a
+# lot more View instances.
+nav.register_element('frontend_top', Navbar(
+    View('KAIST Clubs', '.index'),
+    View('Home', '.index'),
+    # View('Debug-Info', 'debug.debug_root'),
+    # Subgroup(
+    #     'Docs',
+    #     Link('Flask-Bootstrap', 'http://pythonhosted.org/Flask-Bootstrap'),
+    #     Link('Flask-AppConfig', 'https://github.com/mbr/flask-appconfig'),
+    #     Link('Flask-Debug', 'https://github.com/mbr/flask-debug'),
+    #     Separator(),
+    #     Text('Bootstrap'),
+    #     Link('Getting started', 'http://getbootstrap.com/getting-started/'),
+    #     Link('CSS', 'http://getbootstrap.com/css/'),
+    #     Link('Components', 'http://getbootstrap.com/components/'),
+    #     Link('Javascript', 'http://getbootstrap.com/javascript/'),
+    #     Link('Customize', 'http://getbootstrap.com/customize/'), ),
+    # Text('Using Flask-Bootstrap {}'.format(FLASK_BOOTSTRAP_VERSION)),
+    ))
+
+
+# Our index-page just shows a quick explanation. Check out the template
+# "templates/index.html" documentation for more details.
+@frontend.route('/')
+def index():
+    return render_template('index.html')
