f032e617b99fdf9416910b03f71d89332572d217
chaec0803 <chaec0803@gmail.com>
2019-05-26 23:35:36 +0900

    db population

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask import request
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
 
 import os
 import MySQLdb
@@ -8,8 +10,9 @@ import datetime
 
 db = MySQLdb.connect(host = "34.85.123.237", user = "root", passwd = "password", db = "kaistclubdb")
 cur = db.cursor()
+nav = Nav()
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -18,82 +21,130 @@ def index():
 
 @app.route('/eventretrieval')
 def eventretrieval():
-    bob = 5;
     return render_template("eventretrieval.html")
 
-
-"""@app.route('/',methods=["POST"])
-def login():
-    print (request.form)
-    if request.form['type'] == 'signup':
-        email = request.form['email']
-        password = request.form['password1']
-        user_id =  request.form['id']
-        sid = request.form['sid']
-        pnumber = request.form['phone number']
-        try:
-            insert_stmt = (
-                "INSERT INTO admin (ID, PW, Pnumber, Email, SID) VALUES (%s, %s, %s, %s, %s)"
-                )
-            data = (user_id, password, pnumber, email, sid)
-            cur.execute(insert_stmt, data)
-        except sqlite3.IntegrityError:
-            return render_template("signup.html", warning="Sorry, email already taken.")
-
-        db.commit()
-        return render_template("index.html")
-    else:
-        user_id = request.form['id']
-        password = request.form['password']
-        query = "SELECT * from user_info where ID='%s' and PW='%s'" %(user_id.strip(),password.strip())
-        print (query)
-        cur.execute(query)
-        user_info = cur.fetchall()
-        print (user_info)
-        if not user_info:
-            warning = "Incorrect id or password. Please try again."
-            return render_template("index.html", warning=warning)
-        return extract(user_info[0][2])
-
-    cur.execute("SELECT expertise from expertise where id=?", (user_id,))"""
-
-
-@app.route('/clubreg', methods = ['POST'])
+@app.route('/clubreg', methods=['GET'])
 def clubreg():
-    name = request.form['validationCustom01']
-    hours = request.form['validationCustom02']
-    location = request.form['validationCustom03']
-    depatment = request.form['validationCustom04']
+    #getinfo()
+    return render_template("clubreg.html") 
+
+@app.route('/clubreg', methods=['POST'])
+def getinfo():
+    print(request.form)
+    name = request.form['clubname']
+    hours = request.form['clubhours']
+    location = request.form['location']
+    department = request.form['division']
     objective = request.form['Objective']
-
-    departments = ["Culture", "Performing Art", "Creative Art", "Band Music", "Vocal Music", "Instrumental Music", "Society", "Religion", "Ball Sports", "Life Sports", "Science & Engineering", "Liberal Arts"]
-
+    
+    ind = int(department)
+    departments = ["Culture", "Performing Art", "Creative Arts", "Band Music", "Vocal Music", "Instrumental Music", "Society", "Religion", "Ball Sport", "Life Sport", "Science & Engineering", "Liberal Arts"]
 
     insert_stmt = (
-    "INSERT INTO club (Name, MeetingHours, ManagerID, Location, Objective, Dname) VALUES (%s, %s, %s, %s, %s)"
-        )
-    data = (name, hours, '00000', location, objective, departments[department-1])
+    "INSERT INTO club (Name, MeetingHours, ManagerID, Location, Objective, Dname) VALUES (%s, %s, %s, %s, %s, %s)"
+    )
+    data = (name, hours, '00000', location, objective, departments[ind - 1])
 
     cur.execute(insert_stmt, data)
     db.commit()
+    return render_template("index.html")
 
-
-#cur.execute(insert_stmt, data)
-
-"""insert_stmt = (
-    "INSERT INTO admin (ID, PW, Pnumber, Email, SID) VALUES (%s, %s, %s, %s, %s)"
+@app.route('/division')
+def division():
+    select_stmt = (
+        "SELECT * FROM division"
         )
-data = (user_id, password, pnumber, email, sid)
+    cur.execute(select_stmt)
+    divs = cur.fetchall()
+    return render_template("division.html", divs = divs)
 
-cur.execute(insert_stmt, data)
+@app.route('/clubretrieval', methods=['GET'])
+def clubretrieval():
+    div = (request.args.get('div'))
+    try:
+        select_stmt = (
+            "SELECT * FROM club where Dname='%s'" %(div, )
+            )
+        cur.execute(select_stmt)
+        clubs = cur.fetchall()
+        print(clubs)
+        
+        select_stmt1 = (
+        "CREATE VIEW events AS SELECT EventID FROM hostdiv where Division='%s'" %(div, )
+        )
+        select_stmt2 = (
+        "SELECT Name FROM event NATURAL JOIN events"
+        )
+        
+        delete_stmt = (
+        "DROP VIEW events"
+        )
+        cur.execute(select_stmt1)
+        cur.execute(select_stmt2)   
+        event1 = cur.fetchall()
+        print(event1)
+        cur.execute(delete_stmt)
 
-select_stmt = (
-    "SELECT * FROM event"
+        return render_template("clubretrieval.html", div = div, clubs = clubs, events = event1)
+    except MySQLdb.OperationalError:
+        try:
+            select_stmt = (
+                "SELECT * FROM club where Dname='%s'" %(div, )
+                )
+            cur.execute(select_stmt)
+            clubs = cur.fetchall()
+            print(clubs)
+            return render_template("clubretrieval.html", div = div, clubs = clubs)
+        except MySQLdb.OperationalError:
+            return render_template("index.html")
+        
+@app.route('/eventinfo', methods=['GET'])
+def eventinfo():
+    event = (request.args.get('event'))
+    select_stmt = (
+        "SELECT * FROM event where Name='%s'" %(event, )
     )
+    cur.execute(select_stmt)
+    info = cur.fetchall()
+    eventname = info[0][0]
+    datetime = info[0][1]
+    location = info[0][2]
+    
+    return render_template("eventinfo.html", event = event, eventname= eventname, datetime = datetime, location = location)
+#cur.execute(insert_stmt, data)
+  
 
-#cur.execute(select_stmt)
-cur.execute("DELETE FROM event")
-print(cur.fetchall())"""
+@app.route('/clubinfo', methods=['GET'])
+def clubinfo():
+    club = (request.args.get('club'))
+    select_stmt = (
+        "SELECT * FROM club where Name='%s'" %(club, )
+    )
+    cur.execute(select_stmt)
+    info = cur.fetchall()
+    clubname = info[0][0]
+    division = info[0][5]
+    Objective = info[0][4]
+    hours = info[0][1]
+    
+    select_stmt1 = (
+        "CREATE VIEW events AS SELECT EventID FROM hostclub where Club='%s'" %(clubname, )
+    )
+    select_stmt2 = (
+        "SELECT Name FROM event NATURAL JOIN events"
+        )
+    
+    delete_stmt = (
+        "DROP VIEW events"
+        )
+
+    cur.execute(select_stmt1)
+    cur.execute(select_stmt2)   
+    event1 = cur.fetchall()
+    print(event1)
+    cur.execute(delete_stmt)
+    return render_template("clubinfo.html", club = club, clubname= clubname, division = division, Objective = Objective, hours = hours, events = event1)
+#cur.execute(insert_stmt, data)
 
 
 #db.commit()
