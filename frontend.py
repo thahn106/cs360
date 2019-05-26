4cbdc0cdd45aeb3ad903ff29f976faaecae3f3fe
thahn106 <thahn106@gmail.com>
2019-05-27 03:51:27 +0900

    Clean up code and add navbar.

--- frontend.py
+++ frontend.py
@@ -6,11 +6,8 @@
 # http://flask.pocoo.org/docs/blueprints/
 
 from flask import Blueprint, render_template, flash, redirect, url_for, request
-from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
-from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
 from markupsafe import escape
 
-from nav import nav
 
 import os
 import pymysql
@@ -24,26 +21,12 @@ cur = db.cursor()
 # We're adding a navbar as well through flask-navbar. In our example, the
 # navbar has an usual amount of Link-Elements, more commonly you will have a
 # lot more View instances.
-nav.register_element('frontend_top', Navbar(
-    View('KAIST Clubs', '.index'),
-    View('Home', '.index'),
-    View('Club Registration', '.clubreg'),
-    View('New Event', '.newevent'),
-    # View('Debug-Info', 'debug.debug_root'),
-    # Subgroup(
-    #     'Docs',
-    #     Link('Flask-Bootstrap', 'http://pythonhosted.org/Flask-Bootstrap'),
-    #     Link('Flask-AppConfig', 'https://github.com/mbr/flask-appconfig'),
-    #     Link('Flask-Debug', 'https://github.com/mbr/flask-debug'),
-    #     Separator(),
-    #     Text('Bootstrap'),
-    #     Link('Getting started', 'http://getbootstrap.com/getting-started/'),
-    #     Link('CSS', 'http://getbootstrap.com/css/'),
-    #     Link('Components', 'http://getbootstrap.com/components/'),
-    #     Link('Javascript', 'http://getbootstrap.com/javascript/'),
-    #     Link('Customize', 'http://getbootstrap.com/customize/'), ),
-    # Text('Using Flask-Bootstrap {}'.format(FLASK_BOOTSTRAP_VERSION)),
-    ))
+# nav.register_element('frontend_top', Navbar(
+#     View('KAIST Clubs', '.index'),
+#     View('Home', '.index'),
+#     View('Club Registration', '.clubreg'),
+#     View('New Event', '.newevent'),
+#     ))
 
 @frontend.route('/eventretrieval')
 def eventretrieval():
--- main.py
+++ main.py
@@ -1,39 +1,14 @@
 from flask import Flask
-from flask_appconfig import AppConfig
-from flask_bootstrap import Bootstrap
 
 from frontend import frontend
-from nav import nav
-
 
 def create_app(configfile=None):
-    # We are using the "Application Factory"-pattern here, which is described
-    # in detail inside the Flask docs:
-    # http://flask.pocoo.org/docs/patterns/appfactories/
-
     app = Flask(__name__)
-
-    # We use Flask-Appconfig here, but this is not a requirement
-    AppConfig(app)
-
-    # Install our Bootstrap extension
-    Bootstrap(app)
-
-    # Our application uses blueprints as well; these go well with the
-    # application factory. We already imported the blueprint, now we just need
-    # to register it:
     app.register_blueprint(frontend)
-
-    # Because we're security-conscious developers, we also hard-code disabling
-    # the CDN support (this might become a default in later versions):
-    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
-
-    # We initialize the navigation as well
-    nav.init_app(app)
-
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
+    # print("TESTING LOCALLY")
     app.run(host='127.0.0.1', port=8080, debug=True)
